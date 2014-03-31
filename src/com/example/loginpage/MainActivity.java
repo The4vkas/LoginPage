7450cdd6cd501acb91988849de62c99413e80b66
vigneshwaran <vickyanimat@gmail.com>
2014-03-31 13:35:58 +0530

    test lon

---
+++ src/com/example/loginpage/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.loginpage;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Paint;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+TextView _forgot,_sign;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		_forgot=(TextView)findViewById(R.id.forgotpass);
+		_forgot.setPaintFlags(_forgot.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+		_sign=(TextView)findViewById(R.id.signup);
+		_sign.setPaintFlags(_sign.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+		
+	} 
+     public void onclickforgot()
+     {
+    	 // for forgoten password
+    	}
+     public void onclicksign()
+     {
+    	 // for Sign up
+    	}
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
